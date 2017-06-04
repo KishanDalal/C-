fc9beaea38866d7d1e33a52a9f05706decbce2f4
KishanDalal <ikishandalal@gmail.com>
2017-06-04 02:31:02 -0400

    Add files via upload

---
+++ INC/INC/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace INC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Emplyoee Name: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(147, 36);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(183, 35);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(147, 90);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(183, 31);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(89, 190);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(172, 21);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Computer Programmer";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(89, 218);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(159, 21);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Electronics Engineer";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(89, 246);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(98, 21);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Technician";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Hours:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(147, 147);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(183, 27);
+            this.richTextBox3.TabIndex = 9;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 281);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Dependents";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(147, 269);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(183, 29);
+            this.richTextBox4.TabIndex = 11;
+            this.richTextBox4.Text = "";
+            this.richTextBox4.TextChanged += new System.EventHandler(this.richTextBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 398);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+    }
+}
+
---
+++ INC/INC/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; 
+
+namespace INC
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String majorString;
+                String name = richTextBox1.Text;
+                int ID = Convert.ToInt32(richTextBox2.Text);
+
+                bool computerProgrammer = radioButton1.Checked;
+                bool electronicsEngineer = radioButton2.Checked;
+                bool technician = radioButton3.Checked;
+
+                int dependents = Convert.ToInt32(richTextBox4.Text);
+                double Hours = Convert.ToDouble(richTextBox3.Text);
+
+                double socialS = 0.06;
+                double fedTax = 0.14;
+                double provTax = 0.05;
+                double unionFee = 25;
+                double totalD;
+
+                double grossPay;
+                double netPay;
+
+                double cpRate = 35.50;
+                double eeRate = 40.25;
+                double techRate = 37.50;
+
+
+
+                // HOURS LESS THEN 40
+                if (Hours <= 40)
+                {
+                    if (computerProgrammer == true)
+                    {
+                        if (dependents > 3)
+                        {
+                            dependents = dependents * 15;
+                        }
+                        else
+                        {
+                            dependents = 50;
+                        }
+                        grossPay = (cpRate * Hours);
+                        socialS = grossPay * socialS;
+                        fedTax = grossPay * fedTax;
+                        provTax = grossPay * provTax;
+                        netPay = grossPay - socialS - fedTax - provTax - unionFee - dependents;
+
+                        majorString = ("Name: " + name + " Work ID: " + ID + " Gross Pay: $" + grossPay + " Social Security: $" + socialS + " Federal Tax: $" + fedTax + " Provincal Tax: $" + provTax + " Union Fee: $" + unionFee + " Depedents: $" + dependents + " Net pay: $" + netPay);
+                        using (StreamWriter writer = File.AppendText("team.txt"))
+                            writer.WriteLine(majorString);
+                    }
+
+                    if (electronicsEngineer == true)
+                    {
+                        if (dependents > 3)
+                        {
+                            dependents = dependents * 15;
+                        }
+                        else
+                        {
+                            dependents = 50;
+                        }
+                        grossPay = (eeRate * Hours);
+                        socialS = grossPay * socialS;
+                        fedTax = grossPay * fedTax;
+                        provTax = grossPay * provTax;
+                        netPay = grossPay - socialS - fedTax - provTax - unionFee - dependents;
+
+                        majorString = ("Name: " + name + " Work ID: " + ID + " Gross Pay: $" + grossPay + " Social Security: $" + socialS + " Federal Tax: $" + fedTax + " Provincal Tax: $" + provTax + " Union Fee: $" + unionFee + " Dependents: " + dependents + " Net pay: $" + netPay);
+                        using (StreamWriter writer = File.AppendText("team.txt"))
+                            writer.WriteLine(majorString);
+                    }
+
+                    if (technician == true)
+                    {
+                        if (dependents > 3)
+                        {
+                            dependents = dependents * 15;
+                        }
+                        else
+                        {
+                            dependents = 50;
+                        }
+                        grossPay = (techRate * Hours);
+                        socialS = grossPay * socialS;
+                        fedTax = grossPay * fedTax;
+                        provTax = grossPay * provTax;
+                        netPay = grossPay - socialS - fedTax - provTax - unionFee - dependents;
+
+                        majorString = ("Name: " + name + " Work ID: " + ID + " Gross Pay: $" + grossPay + " Social Security: $" + socialS + " Federal Tax: $" + fedTax + " Provincal Tax: $" + provTax + " Union Fee: $" + unionFee + " Dependents: $" + dependents + " Net pay: $" + netPay);
+                        using (StreamWriter writer = File.AppendText("team.txt"))
+                            writer.WriteLine(majorString);
+                    }
+                }
+                // HOURS GREATER THEN 40
+                else if (Hours > 40)
+                {
+                    if (computerProgrammer == true)
+                    {
+                        if (dependents > 3)
+                        {
+                            dependents = dependents * 15;
+                        }
+                        else
+                        {
+                            dependents = 50;
+                        }
+                        grossPay = (cpRate * Hours) + (cpRate * (Hours - 40 * 1.5));
+                        socialS = grossPay * socialS;
+                        fedTax = grossPay * fedTax;
+                        provTax = grossPay * provTax;
+                        netPay = grossPay - socialS - fedTax - provTax - unionFee - dependents;
+
+
+                        majorString = ("Name: " + name + " Work ID: " + ID + " Gross Pay: $" + grossPay + " Social Security: $" + socialS + " Federal Tax: $" + fedTax + " Provincal Tax: $" + provTax + " Union Fee: $" + unionFee + " Depedents: $" + dependents + " Net pay: $" + netPay);
+                        using (StreamWriter writer = File.AppendText("team.txt"))
+                            writer.WriteLine(majorString);
+                    }
+
+                    if (electronicsEngineer == true)
+                    {
+                        if (dependents > 3)
+                        {
+                            dependents = dependents * 15;
+                        }
+                        else
+                        {
+                            dependents = 50;
+                        }
+                        grossPay = (eeRate * Hours) + (100 * Hours - 40);
+                        socialS = grossPay * socialS;
+                        fedTax = grossPay * fedTax;
+                        provTax = grossPay * provTax;
+                        netPay = grossPay - socialS - fedTax - provTax - unionFee - dependents;
+
+                        majorString = ("Name: " + name + " Work ID: " + ID + " Gross Pay: $" + grossPay + " Social Security: $" + socialS + " Federal Tax: $" + fedTax + " Provincal Tax: $" + provTax + " Union Fee: $" + unionFee + " Dependents: $" + dependents + " Net pay: " + netPay);
+                        using (StreamWriter writer = File.AppendText("team.txt"))
+                            writer.WriteLine(majorString);
+                    }
+
+                    if (technician == true)
+                    {
+                        if (dependents > 3)
+                        {
+                            dependents = dependents * 15;
+                        }
+                        else
+                        {
+                            dependents = 50;
+                        }
+                        grossPay = (techRate * Hours) + (techRate * (Hours - 40 * 1.5));
+                        socialS = grossPay * socialS;
+                        fedTax = grossPay * fedTax;
+                        provTax = grossPay * provTax;
+                        netPay = grossPay - socialS - fedTax - provTax - unionFee - dependents;
+
+                        majorString = ("Name: " + name + " Work ID: " + ID + " Gross Pay: $" + grossPay + " Social Security: $" + socialS + " Federal Tax: $" + fedTax + " Provincal Tax: $" + provTax + " Union Fee: $" + unionFee + " Dependents: $" + dependents + " Net pay: $" + netPay + "\n");
+                        using (StreamWriter writer = File.AppendText("team.txt"))
+                            writer.WriteLine(majorString);
+                    }
+
+
+                } // Else statement ends here! 
+                MessageBox.Show("Added Check the Text File: team");
+            }
+            catch
+            {
+                MessageBox.Show("Please make sure ID, Hours and Dependents are numeric.");
+            }
+           
+
+        } // Button ends here 
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
